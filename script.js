ac14181218137b6648a7aa7a6908765e05d92d4e
sourjo-ghosh <joyghosh211@outlook.com>
2026-02-24 00:57:21 +0600

    add id on html and varialbe on js

---
+++ script.js
@@ -0,0 +1,17 @@
+let interviewList = [];
+let rejectedList = [];
+
+let totalCount = document.getElementById("total-count");
+let interviewCount = document.getElementById("interview-count");
+let rejectedCount = document.getElementById("rejected-count");
+
+const allBtn = document.getElementById('all-btn');
+const interviewBtn = document.getElementById('interview-btn');
+const rejectedBtn = document.getElementById('rejected-btn');
+const filteredSection = document.getElementById('filtered-section');
+
+const allCards = document.getElementById('all-cards');
+const showingCount = document.getElementById('showing-count');
+const noJobs = document.getElementById('no-jobs');
+
+const mainContainer = document.querySelector('main');
